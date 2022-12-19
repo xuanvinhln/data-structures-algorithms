a63aaec5161551b38c7d62388540e8c0cc1c7b82
Chu Xuân Vĩnh <xuanvinhln@gmail.com>
2022-12-19 17:11:07 +0700

    Update LeetCode27.java

--- src/lesson1/array/LeetCode27.java
+++ src/lesson1/array/LeetCode27.java
@@ -2,15 +2,16 @@ package lesson1.array;
 
 public class LeetCode27 {
     public int removeElement(int[] nums, int val) {
-        int i = 0;
+        int count = 0;
 
-        for (int j = 0; j < nums.length; j++){
-            if (nums[j] != val){
-                nums[j] = nums[j];
-                i++;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[count] = nums[i];
+                count++;
+                
             }
         }
-
-        return i;
+        
+        return count;
     }
 }
